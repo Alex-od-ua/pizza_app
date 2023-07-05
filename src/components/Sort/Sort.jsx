443871b50239a7c05f2ec8defd974be4524e6763
Alex <ais_alex@ukr.net>
2023-07-05 18:16:54 +0300

    Update Sort.jsx

--- src/components/Sort/Sort.jsx
+++ src/components/Sort/Sort.jsx
@@ -1,6 +1,8 @@
 // import { useNavigate } from 'react-router-dom';
-import styles from './Sort.module.css';
+
 import { useEffect, useState } from 'react';
+
+import styles from './Sort.module.css';
 
 const sortList = [
   { name: 'популярности (DESC)', sortProperty: 'RATING_DESC' },
@@ -9,11 +11,6 @@ const sortList = [
   { name: 'цене (ASC)', sortProperty: 'PRICE_ASC' },
   { name: 'алфавиту (DESC)', sortProperty: 'TITLE_DESC' },
   { name: 'алфавиту (ASC)', sortProperty: 'TITLE_ASC' },
-  //   { name: 'All tweets', type: 'all' },
-  //   { name: 'Max tweets', type: 'tweets' },
-  //   { name: 'Max followers', type: 'followers' },
-  //   { name: 'Follow', type: 'false' },
-  //   { name: 'Followings', type: 'true' },
 ];
 
 export const Sort = ({ value, onChangeCategory, sortByFollowingValue }) => {
@@ -40,28 +37,12 @@ export const Sort = ({ value, onChangeCategory, sortByFollowingValue }) => {
     }
   };
 
-  const onGoBackClick = () => {
-    // navigate('/');
-  };
+  // const onGoBackClick = () => {
+  // navigate('/');
+  // };
 
   return (
     <div className={styles.sort}>
-      {/* <button className={styles.go_back_btn} onClick={onGoBackClick}> */}
-      {/* <svg
-          className={styles.button_svg}
-          width="10"
-          height="8"
-          viewBox="0 0 10 6"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
-            fill="#2C2C2C"
-          />
-        </svg>
-        Go back */}
-      {/* </button> */}
       <div className={styles.sort_label} onClick={() => onClickListItem()}>
         <svg
           className={[styles.sort_svg, open ? styles.sort_svg_open : ''].join(
