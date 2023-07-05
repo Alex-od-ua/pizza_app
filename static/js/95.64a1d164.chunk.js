101d986208b5e0c2572bcce3e80ffe14d444eb3e
Alex-od-ua <108547606+Alex-od-ua@users.noreply.github.com>
2023-07-05 15:18:11 +0000

    Deploying to gh-pages from @ Alex-od-ua/pizza_app@443871b50239a7c05f2ec8defd974be4524e6763 🚀

---
+++ static/js/95.64a1d164.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpizza_app=self.webpackChunkpizza_app||[]).push([[95],{95:function(e,p,a){a.r(p);var n=a(184);p.default=function(){return(0,n.jsx)("div",{children:"Hello"})}}}]);
+//# sourceMappingURL=95.64a1d164.chunk.js.map
